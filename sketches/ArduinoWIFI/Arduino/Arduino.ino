4ddf78fb620287dd46d247ac55320711f00d8775
HJ Cross <github@chimpjuice.com>
2016-12-06 07:38:23 -0500

    rename vars to keeps track of right/left

--- sketches/ArduinoWIFI/Arduino/Arduino.ino
+++ sketches/ArduinoWIFI/Arduino/Arduino.ino
@@ -1,37 +1,40 @@
 #include <SoftwareSerial.h>
- 
-SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
-                             // This means that you need to connect the TX line from the esp to the Arduino's pin 2
-                             // and the RX line from the esp to the Arduino's pin 3
-void setup()
-{
+
+// RX Arduino pin 4 connects to ESP8266's TX pin
+// TX Arduino pin 5 connects to ESP8266's RX pin
+SoftwareSerial esp8266(4,5); 
+
+int EN_RIGHT=9;
+int EN_LEFT=10;
+
+int counterRight = 0;
+void doCountRight() {
+  counterRight++;
+}
+
+int counterLeft = 0;
+void doCountLeft() {
+  counterLeft++;
+}
+
+void setup() {
   Serial.begin(115200);
-  esp8266.begin(115200); // your esp's baud rate might be different
-  esp8266.write("[Arduino Boot]\n");
+  esp8266.begin(115200);
+
+  delay(10000);
+  attachInterrupt(0, doCountRight, RISING);
+  attachInterrupt(1, doCountLeft, RISING);
+  esp8266.write("LEFT, RIGHT\n---------\n");
 }
  
-void loop()
-{
-  if(esp8266.available()){
-    while(esp8266.available())
-    {
-      // The esp has data so display its output to the serial window 
-      char c = esp8266.read(); // read the next character.
-      Serial.write(c);
-    }  
-  }
+void loop() {
+  //TODO: PID for motor speed
+  analogWrite(EN_RIGHT, 255);
+  analogWrite(EN_LEFT, 255);
 
-  if(Serial.available()){
-    delay(1000); 
-    String command="";
-    while(Serial.available()){
-      command+=(char)Serial.read();
-    }
-    esp8266.println(command); // send the read character to the esp8266
-  }
   delay(1000);
-  esp8266.print(analogRead(5));
+  esp8266.print(counterLeft);
   esp8266.print(", ");
-  esp8266.println(analogRead(4));
+  esp8266.println(counterRight);
 }
  
