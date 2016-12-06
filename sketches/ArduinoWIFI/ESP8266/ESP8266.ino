ab1ac8117ba40f9d1cb92391b183e52c06705d2c
HJ Cross <github@chimpjuice.com>
2016-12-06 07:38:54 -0500

    remove useless code from wifi

--- sketches/ArduinoWIFI/ESP8266/ESP8266.ino
+++ sketches/ArduinoWIFI/ESP8266/ESP8266.ino
@@ -3,14 +3,15 @@
 #include <ESP8266WebServer.h>
 #include <SoftwareSerial.h>
 
+// to connect to wifi with password
 #include <credentials.h>
 
 ESP8266WebServer server(80);
 
-SoftwareSerial softSerial(D1, D2); // RX, TX
+SoftwareSerial softSerial(D3, D2); // RX, TX
 
 const int led = LED_BUILTIN;
-String command="A5, A4\n------\n";
+String command="";
 String bufferCommand="";
 
 void handleRoot() {
@@ -18,23 +19,6 @@ void handleRoot() {
   server.send(200, "text/plain", command);
   digitalWrite(led, 0);
   Serial.println("Connected on root");
-}
-
-void handleNotFound(){
-  digitalWrite(led, 0);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 1);
 }
 
 void setup(void){
@@ -58,18 +42,14 @@ void setup(void){
 
   server.on("/", handleRoot);
 
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
-
   server.begin();
+  softSerial.println("WIFI READY");
   Serial.println("HTTP server started");
 }
 
 void loop(void){
   server.handleClient();
+
   if(softSerial.available()){
     delay(1000);
     digitalWrite(led, 1);
